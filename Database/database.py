67249830786d28c441c48d2ff4c611bcbed643ab
EloyGonzalez97 <eloygonzalez07@gmail.com>
2019-03-19 11:37:52 -0400

    Changes to query

--- Database/database.py
+++ Database/database.py
@@ -17,64 +17,52 @@ def create_table(conn, create_table_sql):
     except SyntaxError as e:
         print(e)
 
-
-def create_object(conn, code, object_data):
-        if code == 1:
-            create_insulator(conn, object_data)
-        elif code == 2:
-            create_lighting_arrester(conn, object_data)
-        elif code == 3:
-            create_splice(conn, object_data)
-        elif code == 4:
-            create_disconnect_switch(conn, object_data)
-
-
-def create_insulator(conn, insulator):
+def create_insulator(conn, latitude, longitude, subtype, image_path, created_at, percentage, street_address, city, zip_code):
     """
     Create a new project into the projects table
     :param conn:
     :param insulator:
     """
-    sql = ''' INSERT INTO insulators(subtype,code,condition,image_path, latitude, longitude)
-              VALUES(?,?,?) '''
+    sql = ''' INSERT INTO insulators(latitude, longitude, subtype, image_path, created_at, percentage, street_address, city, zip_code)
+              VALUES(?,?,?,?,?,?,?,?,?) '''
     cur = conn.cursor()
-    cur.execute(sql, insulator)
+    cur.execute(sql, latitude, longitude, subtype, image_path, created_at, percentage, street_address, city, zip_code)
 
 
-def create_lighting_arrester(conn, lightningArrester):
+def create_lightning_arrester(conn, latitude, longitude, subtype, image_path, created_at, percentage, street_address, city, zip_code):
     """
     Create a new project into the projects table
     :param conn:
     :param lightningArrester:
     """
-    sql = ''' INSERT INTO lightning_arresters(subtype,code,condition,image_path, latitude, longitude)
-              VALUES(?,?,?) '''
+    sql = ''' INSERT INTO lightning_arresters(latitude, longitude, subtype, image_path, created_at, percentage, street_address, city, zip_code)
+              VALUES(?,?,?,?,?,?,?,?,?) '''
     cur = conn.cursor()
-    cur.execute(sql, lightningArrester)
+    cur.execute(sql, latitude, longitude, subtype, image_path, created_at, percentage, street_address, city, zip_code)
 
 
-def create_splice(conn, splice):
+def create_splice(conn, latitude, longitude, subtype, image_path, created_at, percentage, street_address, city, zip_code):
     """
     Create a new project into the projects table
     :param conn:
     :param splice:
     """
-    sql = ''' INSERT INTO splices(subtype,code,condition,image_path, latitude, longitude)
-              VALUES(?,?,?) '''
+    sql = ''' INSERT INTO splices(latitude, longitude, subtype, image_path, created_at, percentage, street_address, city, zip_code)
+              VALUES(?,?,?,?,?,?,?,?,?) '''
     cur = conn.cursor()
-    cur.execute(sql, splice)
+    cur.execute(sql, latitude, longitude, subtype, image_path, created_at, percentage, street_address, city, zip_code)
 
 
-def create_disconnect_switch(conn, disconnectSwitch):
+def create_disconnect_switch(conn, latitude, longitude, subtype, image_path, created_at, percentage, street_address, city, zip_code):
     """
     Create a new project into the projects table
     :param conn:
     :param disconnectSwitch:
     """
-    sql = ''' INSERT INTO disconnect_switch(subtype,code,condition,image_path, latitude, longitude)
-              VALUES(?,?,?) '''
+    sql = ''' INSERT INTO disconnect_switch(latitude, longitude, subtype, image_path, created_at, percentage, street_address, city, zip_code)
+              VALUES(?,?,?,?,?,?,?,?,?) '''
     cur = conn.cursor()
-    cur.execute(sql, disconnectSwitch)
+    cur.execute(sql, latitude, longitude, subtype, image_path, created_at, percentage, street_address, city, zip_code)
 
 
 def select_all_object(conn, code):
@@ -92,16 +80,29 @@ def select_all_object(conn, code):
     return rows
 
 
-def select_object_by_condition(conn, code, condition):
+def select_object_by_city(conn, code, city):
     cur = conn.cursor()
     if code == 1:
-        cur.execute("SELECT * FROM insulators WHERE condition=?", (condition,))
+        cur.execute("SELECT * FROM insulators WHERE city=?", (city,))
     elif code == 2:
-        cur.execute("SELECT * FROM lightning_arresters WHERE condition=?", (condition,))
+        cur.execute("SELECT * FROM lightning_arresters WHERE city=?", (city,))
     elif code == 3:
-        cur.execute("SELECT * FROM splices WHERE condition=?", (condition,))
+        cur.execute("SELECT * FROM splices WHERE city=?", (city,))
     elif code == 4:
-        cur.execute("SELECT * FROM disconnect_switch WHERE condition=?", (condition,))
+        cur.execute("SELECT * FROM disconnect_switch WHERE city=?", (city,))
+    rows = cur.fetchall()
+    return rows
+
+def select_object_by_coord(conn, code, lat, long):
+    cur = conn.cursor()
+    if code == 1:
+        cur.execute("SELECT * FROM insulators WHERE latitude=? AND longitude=?", (lat,long))
+    elif code == 2:
+        cur.execute("SELECT * FROM lightning_arresters latitude=? AND longitude=?", (lat,long))
+    elif code == 3:
+        cur.execute("SELECT * FROM splices WHERE latitude=? AND longitude=?", (lat,long))
+    elif code == 4:
+        cur.execute("SELECT * FROM disconnect_switch WHERE latitude=? AND longitude=?", (lat,long))
     rows = cur.fetchall()
     return rows
 
@@ -109,36 +110,52 @@ def select_object_by_condition(conn, code, condition):
 def initialize_db():
 
     sql_create_insulators_table = """CREATE TABLE if not exists insulators (
-                                        subtype text,
-                                        code integer,
-                                        condition text,
-                                        image_path text,
                                         latitude text,
-                                        longitude text
+                                        longitude text,
+                                        subtype text,
+                                        image_path text,
+                                        created_at datetime,
+                                        percentage integer,
+                                        street_address text,
+                                        city text,
+                                        zip_code text,
+                                        PRIMARY KEY (latitude, longitude)
                                 );"""
     sql_create_lightningArresters_table = """ CREATE TABLE if not exists lightning_arresters (
-                                                subtype text,
-                                                code integer,
-                                                condition text,
-                                                image_path text,
                                                 latitude text,
-                                                longitude text
+                                                longitude text,
+                                                subtype text,
+                                                image_path text,
+                                                created_at datetime,
+                                                percentage integer,
+                                                street_address text,
+                                                city text,
+                                                zip_code text,
+                                                PRIMARY KEY (latitude, longitude)
                                         );"""
     sql_create_splices_table = """CREATE TABLE if not exists splices (
+                                    latitude text,
+                                    longitude text,
                                     subtype text,
-                                    code integer,
-                                    condition text,
                                     image_path text,
-                                    latitude real,
-                                    longitude text
+                                    created_at datetime,
+                                    percentage integer,
+                                    street_address text,
+                                    city text,
+                                    zip_code text,
+                                    PRIMARY KEY (latitude, longitude)
                              );"""
     sql_create_disconnectSwitch_table = """ CREATE TABLE if not exists disconnect_switch (
-                                            subtype text,
-                                            code integer,
-                                            condition text,
-                                            image_path text,
-                                            latitude text,
-                                            longitude text
+                                                latitude text,
+                                                longitude text,
+                                                subtype text,
+                                                image_path text,
+                                                created_at datetime,
+                                                percentage integer,
+                                                street_address text,
+                                                city text,
+                                                zip_code text,
+                                                PRIMARY KEY (latitude, longitude)
                                       );"""
 
     conn = create_connection("equipment.db")
